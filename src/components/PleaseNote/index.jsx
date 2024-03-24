f9ed9cfff11397a3cf553bef3b2f1892827681ea
Marley Apong <marlexapong90@gmail.com>
2024-03-24 13:45:38 +0100

    Updates

--- src/components/PleaseNote/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PleaseNote = () => {
-   return (
-      <span className="text-center">Nb : Les champs marqués par des étoiles (<b className="text-danger">*</b>) sont obligatoires</span>
-   )
-}
-
-export default PleaseNote
